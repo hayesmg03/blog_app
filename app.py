f2041e9ce0c7e606cff07b446fa00404356dc073
hayesmg03 <hayesmg03@gmail.com>
2026-03-06 17:10:06 -0500

    SQLAchemy added

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask, render_template
 import os
+import psycopg2
+from flask_sqlalchemy import SQLAlchemy
+
+password = os.environ['DB_PASS']
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']=f'postgresql://postgres:{password}@localhost/5432'
+
+db=SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__='users'
+    user_name=db.Column(db.String(40),primary_key=True)
+    email=db.Column(db.String(40))
+
+    def __init__(self, email):
+        self.email=email
 
 @app.route("/")
 def index():
