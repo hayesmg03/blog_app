7e54fc002d411c087847be47cd1a80b1dae36186
hayesmg03 <hayesmg03@gmail.com>
2026-03-08 22:37:14 -0400

    username properly displays from db

--- app.py
+++ app.py
@@ -8,6 +8,19 @@ load_dotenv()
 
 app = Flask(__name__)
 
+conn = psycopg2.connect(database="postgres",
+                        user='postgres',
+                        password=f'{os.getenv("DB_PASS")}',
+                        host='localhost', port='5432')
+cur = conn.cursor()
+
+cur.execute(
+    "SELECT UserName FROM users WHERE Email = 'email@email.com'"
+)
+
+name = cur.fetchone()[0]
+
+print(name)
 
 @app.route("/")
 def index():
@@ -17,12 +30,14 @@ def index():
                         host='localhost', port='5432')
     cur = conn.cursor()
 
-    name = cur.execute(
+    cur.execute(
         "SELECT UserName FROM users WHERE Email = 'email@email.com'"
     )
+
+    user_name = cur.fetchone()[0]
 
     cur.close()
     conn.close() 
 
-    return render_template('index.html', name=name)
+    return render_template('index.html', user_name=user_name)
 
